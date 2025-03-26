17a7b11cfde1acf5fe8bc5c2408da0ad51740632
noear <noear@live.cn>
2025-03-26 21:26:45 +0800

    新增 solon-flow-magic 插件

--- UPDATE_LOG.md
+++ UPDATE_LOG.md
@@ -2,4 +2,5 @@
 
 * 新增 solon-flow-aviator 插件
 * 新增 solon-flow-beetl 插件
+* 新增 solon-flow-magic 插件
 * 添加 solon-flow Evaluation 接口，为脚本执行解耦
---
+++ solon-flow-projects/solon-flow-magic/src/test/java/features/flow/magic/MagicEvaluationTest.java
@@ -0,0 +1,35 @@
+package features.flow.magic;
+
+import org.junit.jupiter.api.Test;
+import org.noear.solon.flow.ChainContext;
+import org.noear.solon.flow.FlowEngine;
+
+/**
+ * @author noear 2025/3/26 created
+ */
+public class MagicEvaluationTest {
+    @Test
+    public void case1() throws Throwable {
+        FlowEngine engine = FlowEngine.newInstance();
+        engine.register(new MagicDriver());
+
+        engine.load("classpath:flow/*");
+
+        ChainContext context = new ChainContext();
+        context.put("a", 1);
+        context.put("b", 2);
+
+        engine.eval("f1", context);
+        System.out.println(context.result);
+        assert context.result == null;
+
+
+        context = new ChainContext();
+        context.put("a", 3);
+        context.put("b", 2);
+
+        engine.eval("f1", context);
+        System.out.println(context.result);
+        assert ((Number) context.result).intValue() == 5;
+    }
+}
