8d1892b984b4d74057407244e29398730ce2cb82
noear <noear@live.cn>
2025-08-17 17:13:43 +0800

    优化测试代码

--- solon-flow/src/test/java/features/flow/stateful/NotBlockStateFlowTest.java
+++ solon-flow/src/test/java/features/flow/stateful/NotBlockStateFlowTest.java
@@ -23,7 +23,7 @@ public class NotBlockStateFlowTest {
         public void statePut(FlowContext context, Node node, StateType state) {
             super.statePut(context, node, state);
             //todo: 打印放这儿，顺序更真实
-            log.info("{} {} 完成", node.getId(), node.getTitle());
+            log.info("{} 完成", node.getId());
         }
     };
 
@@ -102,66 +102,5 @@ public class NotBlockStateFlowTest {
         statefulNode = statefulService.getTask(chainId, context);
 
         Assertions.assertNull(statefulNode); //全部完成，没有节点可取了
-    }
-
-    @Test
-    public void caseTaskBlock() {
-        FlowEngine flowEngine = FlowEngine.newInstance();
-        FlowStatefulService statefulService = flowEngine.statefulService();
-        Chain chain = getChain();
-
-        FlowContext context = FlowContext.of("3", stateController, stateRepository)
-                .put("tag", "");
-
-        StatefulTask statefulNode = statefulService.getTask(chain, context);
-        Assertions.assertNotNull(statefulNode);
-        Assertions.assertEquals("n3", statefulNode.getNode().getId());
-        Assertions.assertEquals(StateType.COMPLETED, statefulNode.getState());
-
-        context = FlowContext.of("4", stateController, stateRepository)
-                .put("tag", "n1");
-
-        statefulNode = statefulService.getTask(chain, context);
-        Assertions.assertNotNull(statefulNode);
-        Assertions.assertEquals("n1", statefulNode.getNode().getId());
-        Assertions.assertEquals(StateType.WAITING, statefulNode.getState());
-
-        //再跑（仍在原位、原状态）
-        statefulNode = statefulService.getTask(chain, context);
-        Assertions.assertNotNull(statefulNode);
-        Assertions.assertEquals("n1", statefulNode.getNode().getId());
-        Assertions.assertEquals(StateType.WAITING, statefulNode.getState());
-
-
-        context = FlowContext.of("4", stateController, stateRepository)
-                .put("tag", "n2");
-
-        statefulNode = statefulService.getTask(chain, context);
-        Assertions.assertNotNull(statefulNode);
-        Assertions.assertEquals("n2", statefulNode.getNode().getId());
-        Assertions.assertEquals(StateType.WAITING, statefulNode.getState());
-
-        context = FlowContext.of("4", stateController, stateRepository)
-                .put("tag", "");
-
-        statefulNode = statefulService.getTask(chain, context);
-        Assertions.assertNotNull(statefulNode);
-        Assertions.assertEquals("n3", statefulNode.getNode().getId());
-        Assertions.assertEquals(StateType.COMPLETED, statefulNode.getState());
-    }
-
-    private Chain getChain() {
-        Chain chain = new Chain("tmp-" + System.currentTimeMillis());
-
-        String task = "if(tag.equals(node.getId())){exchanger.interrupt();}";
-
-        chain.addNode(NodeDecl.startOf("s").linkAdd("n0"));
-        chain.addNode(NodeDecl.activityOf("n0").task(task).linkAdd("n1"));
-        chain.addNode(NodeDecl.activityOf("n1").task(task).linkAdd("n2"));
-        chain.addNode(NodeDecl.activityOf("n2").task(task).linkAdd("n3"));
-        chain.addNode(NodeDecl.activityOf("n3").task(task).linkAdd("e"));
-        chain.addNode(NodeDecl.endOf("e"));
-
-        return chain;
     }
 }
---
+++ solon-flow/src/test/java/features/flow/stateful/NotBlockStateFlowTest2.java
@@ -0,0 +1,122 @@
+package features.flow.stateful;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.noear.solon.flow.*;
+import org.noear.solon.flow.intercept.ChainInterceptor;
+import org.noear.solon.flow.intercept.ChainInvocation;
+import org.noear.solon.flow.stateful.FlowStatefulService;
+import org.noear.solon.flow.stateful.StateType;
+import org.noear.solon.flow.stateful.StatefulTask;
+import org.noear.solon.flow.stateful.controller.NotBlockStateController;
+import org.noear.solon.flow.stateful.repository.InMemoryStateRepository;
+
+@Slf4j
+public class NotBlockStateFlowTest2 {
+    NotBlockStateController stateController = new NotBlockStateController();
+    InMemoryStateRepository stateRepository = new InMemoryStateRepository() {
+        @Override
+        public void statePut(FlowContext context, Node node, StateType state) {
+            super.statePut(context, node, state);
+            //todo: 打印放这儿，顺序更真实
+            log.info("{} 完成", node.getId());
+        }
+    };
+
+    @Test
+    public void useFlowStatefulService() {
+        FlowEngine flowEngine = FlowEngine.newInstance();
+        FlowStatefulService statefulService = flowEngine.statefulService();
+        Chain chain = getChain();
+
+        FlowContext context = FlowContext.of("3", stateController, stateRepository)
+                .put("tag", "");
+
+        StatefulTask statefulNode = statefulService.getTask(chain, context);
+        System.out.println("--------------------");
+        Assertions.assertNotNull(statefulNode);
+        Assertions.assertEquals("n3", statefulNode.getNode().getId());
+        Assertions.assertEquals(StateType.COMPLETED, statefulNode.getState());
+
+        context = FlowContext.of("4", stateController, stateRepository)
+                .put("tag", "n1");
+
+        statefulNode = statefulService.getTask(chain, context);
+        System.out.println("--------------------");
+        Assertions.assertNotNull(statefulNode);
+        Assertions.assertEquals("n1", statefulNode.getNode().getId());
+        Assertions.assertEquals(StateType.WAITING, statefulNode.getState());
+
+        //再跑（仍在原位、原状态）
+        statefulNode = statefulService.getTask(chain, context);
+        System.out.println("--------------------");
+        Assertions.assertNotNull(statefulNode);
+        Assertions.assertEquals("n1", statefulNode.getNode().getId());
+        Assertions.assertEquals(StateType.WAITING, statefulNode.getState());
+
+
+        context = FlowContext.of("4", stateController, stateRepository)
+                .put("tag", "n2");
+
+        statefulNode = statefulService.getTask(chain, context);
+        System.out.println("--------------------");
+        Assertions.assertNotNull(statefulNode);
+        Assertions.assertEquals("n2", statefulNode.getNode().getId());
+        Assertions.assertEquals(StateType.WAITING, statefulNode.getState());
+
+        context = FlowContext.of("4", stateController, stateRepository)
+                .put("tag", "");
+
+        statefulNode = statefulService.getTask(chain, context);
+        System.out.println("--------------------");
+        Assertions.assertNotNull(statefulNode);
+        Assertions.assertEquals("n3", statefulNode.getNode().getId());
+        Assertions.assertEquals(StateType.COMPLETED, statefulNode.getState());
+    }
+
+    @Test
+    public void useEval() {
+        FlowEngine flowEngine = FlowEngine.newInstance();
+        Chain chain = getChain();
+
+        FlowContext context = FlowContext.of("5", stateController, stateRepository)
+                .put("tag", "");
+
+        flowEngine.eval(chain, context);
+        System.out.println("--------------------");
+
+        context = FlowContext.of("6", stateController, stateRepository)
+                .put("tag", "n1");
+        flowEngine.eval(chain, context);
+        System.out.println("--------------------");
+
+        //再跑（仍在原位、原状态）
+        flowEngine.eval(chain, context);
+        System.out.println("--------------------");
+
+
+        context.put("tag", "n2");
+        flowEngine.eval(chain, context);
+        System.out.println("--------------------");
+
+        context.put("tag", "");
+        flowEngine.eval(chain, context);
+        System.out.println("--------------------");
+    }
+
+    private Chain getChain() {
+        Chain chain = new Chain("tmp-" + System.currentTimeMillis());
+
+        String task = "if(tag.equals(node.getId())){exchanger.interrupt();}";
+
+        chain.addNode(NodeDecl.startOf("s").linkAdd("n0"));
+        chain.addNode(NodeDecl.activityOf("n0").task(task).linkAdd("n1"));
+        chain.addNode(NodeDecl.activityOf("n1").task(task).linkAdd("n2"));
+        chain.addNode(NodeDecl.activityOf("n2").task(task).linkAdd("n3"));
+        chain.addNode(NodeDecl.activityOf("n3").task(task).linkAdd("e"));
+        chain.addNode(NodeDecl.endOf("e"));
+
+        return chain;
+    }
+}
