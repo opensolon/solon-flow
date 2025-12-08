3c4a636133855d9812fa32998f7dc1e0743ba1f5
noear <noear@live.cn>
2025-12-08 22:35:50 +0800

    添加有状态的回流测试

---
+++ solon-flow/src/test/java/features/flow/stateful/BackflowTest.java
@@ -0,0 +1,49 @@
+package features.flow.stateful;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.noear.solon.flow.FlowContext;
+import org.noear.solon.flow.FlowEngine;
+import org.noear.solon.flow.stateful.Operation;
+import org.noear.solon.flow.stateful.StateResult;
+import org.noear.solon.flow.stateful.controller.BlockStateController;
+import org.noear.solon.flow.stateful.repository.InMemoryStateRepository;
+import org.noear.solon.test.SolonTest;
+
+/**
+ *
+ * @author noear 2025/12/8 created
+ *
+ */
+@SolonTest
+public class BackflowTest {
+    private String graphId = "backflow";
+    @Test
+    public void case1() throws Throwable {
+        FlowEngine flowEngine = FlowEngine.newInstance();
+        flowEngine.load("classpath:flow/stateful/*.yml");
+
+        InMemoryStateRepository stateRepository = new InMemoryStateRepository();
+        BlockStateController stateController = new BlockStateController();
+
+        FlowContext flowContext = FlowContext.of("x1", stateController, stateRepository)
+                .put("a",4)
+                .put("b",6);
+
+        StateResult result = flowEngine.forStateful().getTask(graphId, flowContext);
+        System.out.println(result.getNode().getTitle() + " - " + result.getState());
+        Assertions.assertEquals("活动节点1", result.getNode().getTitle());
+
+        flowEngine.forStateful().postOperation(result.getNode(), Operation.FORWARD, flowContext);
+
+        result = flowEngine.forStateful().getTask(graphId, flowContext);
+        System.out.println(result.getNode().getTitle() + " - " + result.getState());
+        Assertions.assertEquals("活动节点3", result.getNode().getTitle());
+
+//        flowEngine.forStateful().postOperation(result.getNode(), Operation.FORWARD, flowContext);
+//
+//        result = flowEngine.forStateful().getTask(graphId, flowContext);
+//        System.out.println(result.getNode().getTitle() + " - " + result.getState());
+//        Assertions.assertEquals("活动节点1", result.getNode().getTitle());
+    }
+}
